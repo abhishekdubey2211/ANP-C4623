bbb8fed9130e9b04e943cd0d80500279383b4390
Abhishek Dubey <100952143+abhishekdubey2211@users.noreply.github.com>
2023-03-01 17:38:49 +0530

    Add files via upload

--- Practicals/TestStudent.java
+++ Practicals/TestStudent.java
@@ -6,11 +6,12 @@ public class TestStudent {
 		// TODO Auto-generated method stub
 		// creating object of class Student
 		Student a = new Student();
+		//student() ia an constructor
 		// object is used to access methods of a class
-		a.setId(7);
+		a.setId(1);
 		a.setName("Abhishek");
 		a.setAge(14);
-		System.out.println("Id:"+a.getId()+"\n"+"Name:"+a.getName()+"\n"+"Age :"+a.getAge());
+		System.out.println("Student Id:"+a.getId()+"\n"+"Student Name:"+a.getName()+"\n"+"Student Age :"+a.getAge());
 	}
 
 }
