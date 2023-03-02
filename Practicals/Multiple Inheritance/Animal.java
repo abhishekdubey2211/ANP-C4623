4b14bcefb1a2bf92eee7e70cf7c8e4a68a82a0eb
Abhishek Dubey <100952143+abhishekdubey2211@users.noreply.github.com>
2023-03-02 22:21:29 +0530

    Add files via upload

---
+++ Practicals/Multiple Inheritance/Animal.java
@@ -0,0 +1,34 @@
+package corejava;
+
+class Animal {
+//method 
+	public void display()
+	{
+		System.out.println("Inside parent class");
+	}
+}
+//child class
+class Dog extends Animal
+{
+public void sound()
+{
+	System.out.println("Barks...");
+}
+}
+class Puppy extends Dog
+{
+	public void run()
+	{
+		System.out.println("He runs by small steps...");
+	}
+}
+class Tester1
+{
+	public static void main(String[] args)
+	{
+		Puppy d=new Puppy();//creating object of child class 
+		d.display();
+		d.sound();
+		d.run();
+	}
+}
