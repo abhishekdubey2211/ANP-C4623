a5c1e253a4fa0f04c42f77f432407bccaf2d2930
Abhishek Dubey <100952143+abhishekdubey2211@users.noreply.github.com>
2023-04-28 13:17:50 +0530

    Add files via upload

---
+++ Selfpractice/loops/LoopsInJava.java
@@ -0,0 +1,18 @@
+package Loops;
+import java.util.*;
+public class LoopsInJava {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		System.out.println("ENTER THE NUMBER OF WHICH U WANT TO PRINT TABLE  = ");
+//		int num1 = sc.nextInt();
+//		
+//		for(int i=0 ; i<11 ; i++) {
+//			System.out.println(num1+"x"+i+" = "+(num1*i));
+//			
+////			System.out.printf("%dx%d = %d\n",num1,i,(num1*i));// using printf
+//		}
+		
+		
+		}
+	}
+}
