ed538c24cd4ca5e12c17a1cdc15db4ec86c799a2
Abhishek Dubey <100952143+abhishekdubey2211@users.noreply.github.com>
2023-05-17 16:45:57 +0530

    Add files via upload

---
+++ SpringProjects/springbootjunit/src/main/java/com/springbootjunit/Springonetoonemapping1Application.java
@@ -0,0 +1,14 @@
+package com.springbootjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springonetoonemapping1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springonetoonemapping1Application.class, args);
+		System.out.println("Welcome to the Movie Show.....");
+	}
+
+}
---
+++ SpringProjects/springbootjunit/src/test/java/com/springbootunit/Springonetoonemapping1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springonetoonemapping1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
