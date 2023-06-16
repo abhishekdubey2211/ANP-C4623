ea9cefe013ce76e043566bdaf9f46fca6affc88a
Abhishek Dubey <100952143+abhishekdubey2211@users.noreply.github.com>
2023-06-17 01:03:31 +0530

    Delete FINAL SPRINGBOOT PROJECT/MovieBookingSystem/MovieBookingSystem directory

--- FINAL SPRINGBOOT PROJECT/MovieBookingSystem/MovieBookingSystem/src/main/java/com/moviebs/service/CustomerService.java
+++
@@ -1,8 +0,0 @@
-package com.moviebs.service;
-import com.moviebs.model.*;
-
-public interface CustomerService {
-	
-	Customer saveCustomer(Customer customer);
-	Integer isCustomerPresent(Customer customer);
-}
